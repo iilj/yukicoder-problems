778ecee8b362b0e724fb1213a94aec714d85fed9
iilj <u.eeoou@gmail.com>
2020-02-16 16:52:40 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          [test] Edit <code>src/App.js</code> and save to reload.
+          [test2] Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
