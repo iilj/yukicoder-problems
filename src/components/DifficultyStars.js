64710d27542830420dffd71b7d18fe9ed33024d5
iilj <u.eeoou@gmail.com>
2020-04-26 11:52:46 +0900

    Add function to calc user level from type0 problems info

--- src/components/DifficultyStars.js
+++ src/components/DifficultyStars.js
@@ -64,3 +64,5 @@ export const DifficultyStarsAbsoluteSpan = (props) => (
     <DifficultyStars {...props} />
   </span>
 );
+
+export const NormalStarElement = (props) => <FontAwesomeIcon icon="star" {...props} />;
