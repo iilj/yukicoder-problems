44c221dab3f73ab7dfe4da378f4d239be1c7b01a
iilj <u.eeoou@gmail.com>
2020-06-16 01:05:26 +0900

    Fix ProblemType filter display label of ListTable

--- src/pages/ListPage/index.tsx
+++ src/pages/ListPage/index.tsx
@@ -18,6 +18,7 @@ import { useParams } from 'react-router-dom';
 import { DifficultyLevelTable } from './DifficultyLevelTable';
 import { ProblemTypeTable } from './ProblemTypeTable';
 import { ListTable, FilterState } from './ListTable';
+import { getProblemTypeName } from '../../utils';
 import * as TypedCachedApiClient from '../../utils/TypedCachedApiClient';
 import { WellPositionedDropdownMenu } from '../../components/WellPositionedDropdownMenu';
 import {
@@ -273,7 +274,11 @@ export const ListPage = (): JSX.Element => {
 
         <ButtonGroup className="mr-4">
           <UncontrolledDropdown>
-            <DropdownToggle caret>{problemTypeFilterState}</DropdownToggle>
+            <DropdownToggle caret>
+              {problemTypeFilterState === 'All'
+                ? 'All'
+                : getProblemTypeName(problemTypeFilterState)}
+            </DropdownToggle>
             <DropdownMenu>
               <DropdownItem onClick={() => setProblemTypeFilterState('All')}>
                 All
