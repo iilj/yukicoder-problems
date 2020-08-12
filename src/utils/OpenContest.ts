7bc5f8a89de742d12c9633398d34dc005c7e1fcb
iilj <u.eeoou@gmail.com>
2020-08-13 01:43:27 +0900

    Add Nada 75th contest imported from HackerRank

--- src/utils/OpenContest.ts
+++ src/utils/OpenContest.ts
@@ -1,6 +1,31 @@
 import { Contest } from '../interfaces/Contest';
 
 export const OpenContests: Contest[] = [
+  // https://www.hackerrank.com/75th
+  // id: 97235
+  // get_endtimeiso: "2020-03-19T14:00:00Z"
+  // get_starttimeiso: "2020-03-19T11:30:00Z"
+  {
+    Id: -97235,
+    Name: '灘校75回生中学卒業記念コンテスト (Open)',
+    Date: '2020-03-19T20:30:00+09:00',
+    EndDate: '2020-03-19T23:00:00+09:00',
+    ProblemIdList: [
+      4951,
+      4952,
+      4954,
+      4956,
+      4957,
+      4958,
+      4959,
+      4960,
+      4961,
+      4962,
+      4963,
+      4964,
+      4965,
+    ],
+  },
   // https://www.hackerrank.com/epsf001
   // id: 69694
   // get_endtimeiso: "2019-10-14T08:30:00Z"
